68f37ba432b8681d751e4a0e52770a3c75735e65
augustoserra-edu <augusto.serra@comunidad.ub.edu.ar>
2026-05-06 17:43:36 -0300

    habia un error al hacer el test, se resolvio modifcando el archivo calculator.js en la function subtract

--- calculator.js
+++ calculator.js
@@ -1,4 +1,11 @@
 export function subtract(a, b) {
     // ERROR INTENCIONAL: simula un bug de precisión flotante
-    return a - b + 0.0001;
+    return a - b ;
 }
+
+export function calcularPrecio(precio) {
+if (precio >= 1000) {
+// Corrección: se agrega el signo '='
+return precio * 0.9;
+}
+return precio; }
